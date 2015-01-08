e7a87bd1b1d3c381f7cc431bfd4db0962dbfd8c6
irufus <sakamura@gmail.com>
2015-01-08 01:04:08 -0600

    Fully functional miner

--- TJMediaMiner/home/irufus/TJMediaMiner/TJMediaMiner.java
+++ TJMediaMiner/home/irufus/TJMediaMiner/TJMediaMiner.java
@@ -5,6 +5,10 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 
 import org.jsoup.Jsoup;
@@ -17,29 +21,49 @@ import org.jsoup.select.Elements;
 public class TJMediaMiner {
 
 	public static void main(String[] args) {
-		String url = "http://www.tjmedia.co.kr/tjsong/song_monthNew.asp?YY=2014&MM=01";
+		SimpleDateFormat month = new SimpleDateFormat("MM");
+		SimpleDateFormat year = new SimpleDateFormat("YYYY");
+		Calendar cal = new GregorianCalendar(2007,0,1);
+		Calendar now = Calendar.getInstance();
+		now.add(Calendar.MONTH, 1);
+		
+		String url = "http://www.tjmedia.co.kr/tjsong/song_monthNew.asp?YY=%s&MM=%s";
 		try 
 		{
-			Document doc = Jsoup.parse(new URL(url).openStream(), "ISO-8859-1", url);
-			Elements table = doc.select("table");
-			Iterator<Element> ite = table.select("tr").iterator();
-			PrintWriter pw = new PrintWriter("test.dat", "ISO-8859-1");
-			while(ite.hasNext())
+			PrintWriter pw = new PrintWriter("songs.dat", "ISO-8859-1");
+			while(cal.compareTo(now) < 0)
 			{
-//				System.out.println(ite.next().text());
-				pw.println(ite.next().text());				
+				System.out.println("Getting info for: " + year.format(cal.getTime()) + "-" + month.format(cal.getTime()));
+				String formatedURL = String.format(url, new String[] { year.format(cal.getTime()), month.format(cal.getTime()) });
+				System.out.println(formatedURL);
+				Document doc = Jsoup.parse(new URL(formatedURL).openStream(), "ISO-8859-1", url);
+				Elements table = doc.select("table.board_type1");
+				Iterator<Element> ite = table.select("tr").iterator();				
+				while(ite.hasNext())
+				{
+					Element tr = ite.next();
+					Iterator<Element> tds = tr.select("td").iterator();
+					while(tds.hasNext())
+					{
+						String entry = tds.next().text();
+						if(!tds.hasNext())
+							pw.print(entry);						
+						else
+							pw.print(entry + ",");
+					}
+					pw.println();		
+				}
+				cal.add(Calendar.MONTH, 1);
 			}
 			pw.close();
-			
-		} 
+		}
 		catch (IOException e) 
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		
-		
+			
 	}
 
 }
